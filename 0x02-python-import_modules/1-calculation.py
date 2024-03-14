1061872082d682639c57ea54d5122e0227f9a1f8
BOULAKNADEL <boulaknadelhamza@gmail.com>
2024-03-14 17:44:50 +0000

    My first toolbox!

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import calculator_1
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, calculator_1.add(a, b)), end=" ")
+    print()
+    print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)), end=" ")
+    print()
+    print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)), end=" ")
+    print()
+    print("{} / {} = {}".format(a, b, calculator_1.div(a, b)), end=" ")
+print()
