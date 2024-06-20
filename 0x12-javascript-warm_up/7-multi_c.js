628ee054e63dc89b260bcfe98890b35710d33485
BOULAKNADEL <boulaknadelhamza@gmail.com>
2024-06-20 22:59:36 +0100

    I Love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ const nTimes = parseInt(process.argv[2]);
 if (!isNaN(nTimes)) {
   if (nTimes > 0) {
     for (let i = 0; i < nTimes; i++) {
-	  process.stdout.write('C is fun\n');
+      process.stdout.write('C is fun\n');
     }
   } else {
     console.log('Missing Number of occurrences');
