1678f749b8310fdf79f0fb1c4053ae65b748fd19
BOULAKNADEL <boulaknadelhamza@gmail.com>
2024-06-20 21:58:30 +0100

    to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const arg1 = parseFloat(process.argv[2]);
+const arg1 = parseInt(process.argv[2]);
 
 if (!isNaN(arg1)) {
-  console.log(`My number: ${parseInt(arg1)}`);
+  console.log(`My number: ${(arg1)}`);
 } else {
-  console.log('No argument');
+  console.log('Not a number');
 }
