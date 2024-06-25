30b1b986a3fe8fcb4cb9ebaa3fe38cd48b89d1f0
BOULAKNADEL <boulaknadelhamza@gmail.com>
2024-06-26 00:00:58 +0100

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,33 +1,9 @@
 #!/usr/bin/node
 
-function secondBiggest (args, length) {
-  let max1;
-  let max2;
-
-  if (length <= 3) {
-    return 0;
-  }
-
-  if (args[2] > args[3]) {
-    max1 = args[2];
-    max2 = args[3];
-  } else {
-    max1 = args[3];
-    max2 = args[2];
-  }
-
-  for (let i = 2; i < length; i++) {
-    if (args[i] > max1) {
-      max2 = max1;
-      max1 = args[i];
-    } else if (args[i] > max2 && args[i] < max1) {
-      max2 = args[i];
-    }
-  }
-  return max2;
+const args = process.argv.slice(2).map(Number);
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  args.sort((a, b) => b - a);
+  console.log(args[1]);
 }
-
-const args = process.argv;
-const length = args.length;
-const begSec = secondBiggest(args, length);
-console.log(begSec);
